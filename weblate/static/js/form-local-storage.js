7fd44311a9641b88b19f3808fd0e3dcedc46ae0b
Marc Perrin <marc.perrin@dont-nod.com>
2017-06-26 14:50:35 +0200

    [SEARCH] Local storage persistence for search forms (all over the website, but per location pathname)

---
+++ weblate/static/js/form-local-storage.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    if (window.localStorage) {
+        // Load from local storage:
+        id = 'searchForm_' + location.pathname;
+        storedValue = localStorage[id];
+        if (storedValue) {
+            persistenceData = JSON.parse(storedValue);
+            $(".textinput,.select", "form.searchform").each(function () {
+                $(this).val(persistenceData[this.id]);
+            });
+            $(".checkboxinput", "form.searchform").each(function () {
+                $(this).prop('checked', persistenceData[this.id]);
+            });
+        }
+        // Save to local storage:
+        $('form.searchform').submit(function (e) {
+            persistenceData = new Object();
+            $(".textinput,.select", "form.searchform").each(function () {
+                persistenceData[this.id] = $(this).val();
+            });
+            $(".checkboxinput", "form.searchform").each(function () {
+                persistenceData[this.id] = $(this).prop('checked');
+            });
+            localStorage[id] = JSON.stringify(persistenceData);
+        });
+    }
+});
